e7893cd866823e3514a99accc67f18c5833c5fda
Cloud Technet <69082631+cloudtechnet@users.noreply.github.com>
2026-06-29 09:53:36 +0530

    new java project commit

---
+++ src/main/java/com/cloudtechnet/app.java
@@ -0,0 +1,12 @@
+package com.cloudtechnet;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CloudTechnet!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
